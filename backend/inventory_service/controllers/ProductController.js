98840e86ed2004801d9ccd715a6a653d045ce4c8
nooblyf12345 <zubair18299@outlook.com>
2023-09-18 06:50:48 +0500

    async stuff

--- backend/inventory_service/controllers/ProductController.js
+++ backend/inventory_service/controllers/ProductController.js
@@ -1,12 +1,12 @@
 const {Product} = require('../models/Product')
 const getProducts= async function(){
     const data = await Product.find({});
-    console.log(data)
+    console.log('db data for product : : ',data)
     return data;
 }
 const createProducts= async function(){
     const data = await Product.create({ size: 'small' });
-    console.log(data)
+    console.log('db data for product : : ',data)
     return data;
 }
 
--- backend/inventory_service/routes/ProductsRoute.js
+++ backend/inventory_service/routes/ProductsRoute.js
@@ -1,12 +1,14 @@
 const express=require('express')
 const router=express.Router()
 const ProductController = require('../controllers/ProductController');
-router.get("/",(req,res)=>{
-    const data = ProductController.getProducts();
+router.get("/",async (req,res)=>{
+    const data = await ProductController.getProducts();
+    console.log('route data: ', data);
     res.send(data)
 })
-router.post("/",(req,res)=> {
-    const data = ProductController.createProducts();
+router.post("/",async(req,res)=> {
+    const data = await ProductController.createProducts();
+    console.log('route data: ', data);
     res.send(data)
 })
 module.exports = router;
