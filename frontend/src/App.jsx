7a126cfc577854a0d94aae611bc6e5ed4b44c285
fakhirsh <fakhir.shaheen@gmail.com>
2023-09-25 12:14:29 +0500

    Basic working in realtime

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,17 +4,41 @@ import Experience from './Experience'
 
 function App() {
 
+
+  /*
+  Position:  
+    x: -0.5763754905804962, 
+    -0.38282324342312785, 
+    1.249279955928787
+
+  Rotation:  
+    isEuler: true, 
+    _x: 0.01920760670291659, 
+    _y: -0.31541408186142517, 
+    _z: 0.005959056917360834, 
+    _order: 'XYZ'
+  */
+
+
   return (
     <div className='bg-indigo-800 absolute w-full h-screen p-0 top-0 left-0'>
       <Canvas
         flat
         camera={{ 
-          position: [0, 0.1, 2.5],
-          fov: 70,
+          //position: [3.34485 , 0.689949 , 1.04566 ],
+          fov: 36,
+          //lookAt: [-2.81447, 0.541734, 0.944357],
         }}
       >
         <Experience />
       </Canvas>
+
+      {/* <EffectComposer>
+        <DepthOfField focusDistance={0} focalLength={0.02} bokehScale={2} height={480} />
+        <Bloom luminanceThreshold={0} luminanceSmoothing={0.9} height={300} />
+        <Noise opacity={0.02} />
+        <Vignette eskil={false} offset={0.1} darkness={1.1} />
+      </EffectComposer> */}
     </div>
   )
 }
--- frontend/src/Experience.jsx
+++ frontend/src/Experience.jsx
@@ -1,125 +1,101 @@
-import { Center, Environment, OrbitControls, Sparkles, shaderMaterial, useGLTF, useTexture, Text, Text3D, useMatcapTexture, Outlines } from "@react-three/drei";
-import { extend, useFrame } from "@react-three/fiber";
+import { Center, Environment, OrbitControls, Sparkles, shaderMaterial, useGLTF, useTexture, Text, Text3D, useMatcapTexture, Outlines, Sky } from "@react-three/drei";
+import { Bloom, DepthOfField, EffectComposer, Noise, Vignette } from '@react-three/postprocessing'
+import { extend, useFrame, useThree } from "@react-three/fiber";
+import { useControls } from "leva";
 import { Perf } from "r3f-perf";
 import { useEffect, useRef, useState } from "react";
 import * as THREE from 'three';
 
-import portalVertexShader from './shaders/portal/vertex.glsl';
-import portalFragmentShader from './shaders/portal/fragment.glsl';
-
-const PortalMaterial = shaderMaterial(
-    {
-        uTime: 0,
-        uColorStart: new THREE.Color('#ffffff'),
-        uColorEnd: new THREE.Color('#000000'),
-    },
-    portalVertexShader,
-    portalFragmentShader
-)
-extend({ PortalMaterial });
-
 export default function Experience() {
 
-    const {nodes} = useGLTF('./portal.glb');
-    const [axeSelected, setAxeSelected] = useState(false);
-    const matcapTexture = useMatcapTexture('E6BF3C_5A4719_977726_FCFC82', 256);
+    const {nodes} = useGLTF('./corplux_stage.glb');
+    //console.log(nodes);
+    const stageDiffuse = useTexture('./stage_diffuse.jpg');
+    stageDiffuse.flipY = false;
 
-    const bakedTexture = useTexture('./baked.jpg');
-    bakedTexture.flipY = false;
+    const mesh1 = useGLTF('./interactables.glb');
+    console.log(mesh1.nodes);
 
-    const portalMaterial = useRef();
+    const { camera } = useThree();
 
-    useFrame((_, dt) => {
-        portalMaterial.current.uniforms.uTime.value += dt;
+    const {pos_x, pos_y, pos_z, rot_x, rot_y, rot_z, scale, sunPosition} = useControls({
+        pos_x: { value: 0, min: -10, max: 10, step: 0.01 },
+        pos_y: { value: 0, min: -10, max: 10, step: 0.01 },
+        pos_z: { value: 0, min: -10, max: 10, step: 0.01 },
+        rot_x: { value: 0, min: -10, max: 10, step: 0.01 },
+        rot_y: { value: 0, min: -10, max: 10, step: 0.01 },
+        rot_z: { value: 0, min: -10, max: 10, step: 0.01 },
+        scale: { value: 1, min: -10, max: 10, step: 0.01 },
+        sunPosition: { value: [ 1, 2, 3 ] },
     });
+
+    console.log("Position: ", pos_x, pos_y, pos_z, "\nRotation: ", rot_x, rot_y, rot_z, "\nScale: ", scale);
+
+    // useFrame(() => {
+    //     console.log(camera);
+    //   },[]);
 
     return( 
         <>
             <color attach='background' args={['#030202']} />
-            {/* <Perf position="top-left" /> */}
+            <Perf position="top-left" />
             
-            <Text3D
-                    font="./font1.json"
-                    size={0.75}
-                    height={0.2}
-                    position={[-1.8, 3, -4]}
-                    curveSegments={12}
-                    bevelEnabled
-                    bevelThickness={0.03}
-                    bevelSize={0.02}
-                    bevelOffset={0}
-                    bevelSegments={5}
-                >
-                    Corplux
-                    {/* <meshMatcapMaterial matcap={matcapTexture} /> */}
-                    <meshNormalMaterial />
-                </Text3D>
-
-            <OrbitControls 
-                makeDefault
-                enablePan={false}
-                enableZoom={false}
-                minPolarAngle={Math.PI / 4}  // radians
-                maxPolarAngle={Math.PI / 1.8}  // radians
-            >
-                {/* <perspectiveCamera position={[0, -2, 1]} /> */}
+            <OrbitControls makeDefault >
             </OrbitControls>
-            <Center>
-                <mesh 
-                    geometry={nodes.baked.geometry}
-                    scale={[2, 2, 2]}
-                >    
-                    <meshBasicMaterial map={bakedTexture} />
-                </mesh>
-                <mesh 
-                    geometry={nodes.poleLightA.geometry} 
-                    rotation={nodes.poleLightA.rotation}
-                    position={nodes.poleLightA.position}
-                    
-                />
-                <mesh 
-                    geometry={nodes.poleLightB.geometry}
-                    rotation={nodes.poleLightB.rotation}
-                    position={nodes.poleLightB.position}
-                
-                />
-                <mesh 
-                    geometry={nodes.portalLight.geometry} 
-                    rotation={nodes.portalLight.rotation}
-                    position={nodes.portalLight.position}
-                >
-                    <portalMaterial ref={portalMaterial}/>
-                </mesh>
-                <mesh 
-                    geometry={nodes.axe.geometry}
-                    rotation={nodes.axe.rotation}
-                    position={nodes.axe.position}
-                    onPointerOver={() => setAxeSelected(true)}
-                    onPointerOut={() => setAxeSelected(false)}
-                >
-                    <meshBasicMaterial map={bakedTexture} />
-                    {axeSelected && (
-                        <Outlines thickness={0.01} color="orange" />
-                    )}
-                </mesh>
-                {axeSelected && (
-                    < TextWithBackground position={nodes.axe.position} />
-                )}
-                
-                
 
-                <Sparkles 
-                    size={6}
-                    scale={[4,2,4]}
-                    position-y = {1}
-                    speed={0.2}
-                    count={40}
-                />
+            <Center>
+                <mesh geometry={nodes.stage.geometry}>
+                    <meshStandardMaterial map={stageDiffuse}/>
+                </mesh>
+                <mesh 
+                    geometry={mesh1.nodes.tv_screen.geometry}
+                    position={[-0.68, -0.24, 1.53]}
+                    rotation={[0, -1.57, 0 ]}
+                    scale={[0.58, 0.58, 0.58]}
+                >
+                    <meshBasicMaterial />
+                </mesh>
+                <mesh 
+                    geometry={mesh1.nodes.btn_base.geometry}
+                    position={[-0.74, -0.28, 1.62]}
+                    rotation={[0, -1.57, 0 ]}
+                    scale={[0.6, 0.6, 0.6]}
+                    onClick={(e) => {
+                        // Handle click event
+                        console.log('Clicked on btn_base!');
+                    }}
+                    onPointerOver={(e) => {
+                        // Handle hover over event
+                        console.log('Mouse over btn_base');
+                    }}
+                    onPointerOut={(e) => {
+                        // Handle hover out event
+                        console.log('Mouse out of btn_base');
+                    }}
+                >
+                    <meshStandardMaterial color="red"/>
+                </mesh>
+                <mesh 
+                    geometry={mesh1.nodes.btn_text.geometry}
+                    position={[-0.74, -0.28, 1.62]}
+                    rotation={[0, -1.57, 0 ]}
+                    scale={[0.6, 0.6, 0.6]}
+                >
+                    <meshStandardMaterial color="white"/>
+                </mesh>
             </Center>
-            {/* <Environment 
+            <Environment 
                 background
-                preset="night" 
-            /> */}
+                preset="apartment"
+            />
+            <Sky sunPosition={ sunPosition } />
+
+            <EffectComposer>
+                {/* <DepthOfField focusDistance={0} focalLength={0.02} bokehScale={2} height={480} /> */}
+                <Bloom luminanceThreshold={1} luminanceSmoothing={0.9} height={300} />
+                <Noise opacity={0.04} />
+                <Vignette eskil={false} offset={0.01} darkness={0.9} />
+            </EffectComposer>
 
         </>
     );
@@ -143,6 +119,7 @@ function TextWithBackground({ position }) {
         <planeGeometry args={[0.5, 0.2]} />
         <meshBasicMaterial color="#000000" transparent opacity={0.7} />
       </mesh>
+
       </>
     );
   }
