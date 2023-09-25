812a1b840fb64a6fdde48c280dd592dc278e0f01
fakhirsh <fakhir.shaheen@gmail.com>
2023-09-25 16:48:46 +0500

    Main page 3D graphics done. big screens only

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,20 +4,10 @@ import Experience from './Experience'
 
 function App() {
 
-
-  /*
-  Position:  
-    x: -0.5763754905804962, 
-    -0.38282324342312785, 
-    1.249279955928787
-
-  Rotation:  
-    isEuler: true, 
-    _x: 0.01920760670291659, 
-    _y: -0.31541408186142517, 
-    _z: 0.005959056917360834, 
-    _order: 'XYZ'
-  */
+/*
+Position: _Vector3 {x: -1.137768585447983, y: -0.2835070983595434, z: 1.6360301129911816} 
+Rotation:  _Euler {isEuler: true, _x: -0.0040239456620568635, _y: -0.4830358088251831, _z: -0.0018690091101864709, 
+*/
 
 
   return (
@@ -25,20 +15,14 @@ function App() {
       <Canvas
         flat
         camera={{ 
-          //position: [3.34485 , 0.689949 , 1.04566 ],
-          fov: 36,
+          //position: [-1.137768585447983, -0.2835070983595434, 1.6360301129911816 ],
+          //rotation: [-0.0040239456620568635, -0.4830358088251831, -0.0018690091101864709],
+          //fov: 36,
           //lookAt: [-2.81447, 0.541734, 0.944357],
         }}
       >
         <Experience />
       </Canvas>
-
-      {/* <EffectComposer>
-        <DepthOfField focusDistance={0} focalLength={0.02} bokehScale={2} height={480} />
-        <Bloom luminanceThreshold={0} luminanceSmoothing={0.9} height={300} />
-        <Noise opacity={0.02} />
-        <Vignette eskil={false} offset={0.1} darkness={1.1} />
-      </EffectComposer> */}
     </div>
   )
 }
--- frontend/src/Experience.jsx
+++ frontend/src/Experience.jsx
@@ -1,35 +1,100 @@
-import { Center, Environment, OrbitControls, Sparkles, shaderMaterial, useGLTF, useTexture, Text, Text3D, useMatcapTexture, Outlines, Sky } from "@react-three/drei";
+import { Center, Environment, OrbitControls, Sparkles, shaderMaterial, useGLTF, useTexture, Text, Text3D, useMatcapTexture, Outlines, Sky, useVideoTexture, Float, PresentationControls } from "@react-three/drei";
 import { Bloom, DepthOfField, EffectComposer, Noise, Vignette } from '@react-three/postprocessing'
 import { extend, useFrame, useThree } from "@react-three/fiber";
 import { useControls } from "leva";
 import { Perf } from "r3f-perf";
 import { useEffect, useRef, useState } from "react";
 import * as THREE from 'three';
+import { useSpring, animated } from "react-spring";
+
+const LAPTOP_FOV = 36;  // Replace 50 with your desired landscape FOV value
+const MOBILE_FOV = 55;
 
 export default function Experience() {
-
+    
     const {nodes} = useGLTF('./corplux_stage.glb');
     //console.log(nodes);
     const stageDiffuse = useTexture('./stage_diffuse.jpg');
     stageDiffuse.flipY = false;
 
     const mesh1 = useGLTF('./interactables.glb');
-    console.log(mesh1.nodes);
+    // console.log(mesh1.nodes);
+
+    const videoTexture = useVideoTexture("/ad.mp4")
+    // console.log(videoTexture);
+
+    // const {pos_x, pos_y, pos_z, rot_x, rot_y, rot_z, s_x, s_y, s_z, sunPosition} = useControls({
+    //     pos_x: { value: 0, min: -10, max: 10, step: 0.01 },
+    //     pos_y: { value: 0, min: -10, max: 10, step: 0.01 },
+    //     pos_z: { value: 0, min: -10, max: 10, step: 0.01 },
+    //     rot_x: { value: 0, min: -10, max: 10, step: 0.01 },
+    //     rot_y: { value: 0, min: -10, max: 10, step: 0.01 },
+    //     rot_z: { value: 0, min: -10, max: 10, step: 0.01 },
+    //     s_x: { value: 1, min: -10, max: 10, step: 0.01 },
+    //     s_y: { value: 1, min: -10, max: 10, step: 0.01 },
+    //     s_z: { value: 1, min: -10, max: 10, step: 0.01 },
+    //     sunPosition: { value: [ 1, 2, 3 ] },
+    // });
+
+    // console.log("Position: ", pos_x, pos_y, pos_z, "\nRotation: ", rot_x, rot_y, rot_z, "\nScale: ", s_x, s_y, s_z, "\nSun Position: ", sunPosition);
+
+/*
+, y: , z: } 
+Rotation:  _Euler {isEuler: true, _x: -0.029723520944287127, _y: -0.5602254619358139, _z: -0.0157977402701478
+*/
 
     const { camera } = useThree();
+    useEffect(() => {
+        camera.position.set(-1.112505147567413, -0.34148052724047506, 1.9676561805147699);
+        camera.quaternion.set(0.02293597168506244, -0.2156830279090632, 0.005067605779075498, 0.9761808705596049)
+        camera.updateProjectionMatrix();  // Update the camera's projection matrix after changing position
+    }, []);
 
-    const {pos_x, pos_y, pos_z, rot_x, rot_y, rot_z, scale, sunPosition} = useControls({
-        pos_x: { value: 0, min: -10, max: 10, step: 0.01 },
-        pos_y: { value: 0, min: -10, max: 10, step: 0.01 },
-        pos_z: { value: 0, min: -10, max: 10, step: 0.01 },
-        rot_x: { value: 0, min: -10, max: 10, step: 0.01 },
-        rot_y: { value: 0, min: -10, max: 10, step: 0.01 },
-        rot_z: { value: 0, min: -10, max: 10, step: 0.01 },
-        scale: { value: 1, min: -10, max: 10, step: 0.01 },
-        sunPosition: { value: [ 1, 2, 3 ] },
+    useEffect(() => {
+        const handleResize = () => {
+            if (window.innerHeight > window.innerWidth) {
+                // It's portrait
+                camera.fov = MOBILE_FOV; // e.g., 75 or whatever works for your scene
+            } else {
+                // It's landscape
+                camera.fov = LAPTOP_FOV; // e.g., 50
+            }
+            camera.aspect = window.innerWidth / window.innerHeight;
+            camera.updateProjectionMatrix();
+        };
+    
+        window.addEventListener('resize', handleResize);
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, [camera]);
+    
+    
+    // useEffect(() => {
+    //     function logCameraPosition() {
+    //         console.log("Position:", camera.position, "\nRotation: ", camera.rotation);
+    //         requestAnimationFrame(logCameraPosition);
+    //     }
+        
+    //     logCameraPosition();
+
+    //     // Cleanup function to stop logging when component is unmounted
+    //     return () => cancelAnimationFrame(logCameraPosition);
+    // }, [camera]);
+    // console.log(camera);
+
+     
+    const [hovered, setHovered] = useState(false);
+
+    const btnZPos = 1.62;
+    const textPos = useState([-0.74, -0.28, 1.62]);
+
+    const btnAnimProps = useSpring({
+        z: hovered ? btnZPos+0.5 : btnZPos
     });
 
-    console.log("Position: ", pos_x, pos_y, pos_z, "\nRotation: ", rot_x, rot_y, rot_z, "\nScale: ", scale);
+    //console.log(btnPos);
+    // console.log(btnAnimProps.z.get());
 
     // useFrame(() => {
     //     console.log(camera);
@@ -38,52 +103,68 @@ export default function Experience() {
     return( 
         <>
             <color attach='background' args={['#030202']} />
-            <Perf position="top-left" />
+            {/* <Perf position="top-left" /> */}
             
-            <OrbitControls makeDefault >
-            </OrbitControls>
-
-            <Center>
-                <mesh geometry={nodes.stage.geometry}>
-                    <meshStandardMaterial map={stageDiffuse}/>
-                </mesh>
-                <mesh 
-                    geometry={mesh1.nodes.tv_screen.geometry}
-                    position={[-0.68, -0.24, 1.53]}
-                    rotation={[0, -1.57, 0 ]}
-                    scale={[0.58, 0.58, 0.58]}
-                >
-                    <meshBasicMaterial />
-                </mesh>
-                <mesh 
-                    geometry={mesh1.nodes.btn_base.geometry}
-                    position={[-0.74, -0.28, 1.62]}
-                    rotation={[0, -1.57, 0 ]}
-                    scale={[0.6, 0.6, 0.6]}
-                    onClick={(e) => {
-                        // Handle click event
-                        console.log('Clicked on btn_base!');
-                    }}
-                    onPointerOver={(e) => {
-                        // Handle hover over event
-                        console.log('Mouse over btn_base');
-                    }}
-                    onPointerOut={(e) => {
-                        // Handle hover out event
-                        console.log('Mouse out of btn_base');
-                    }}
-                >
-                    <meshStandardMaterial color="red"/>
-                </mesh>
-                <mesh 
-                    geometry={mesh1.nodes.btn_text.geometry}
-                    position={[-0.74, -0.28, 1.62]}
-                    rotation={[0, -1.57, 0 ]}
-                    scale={[0.6, 0.6, 0.6]}
-                >
-                    <meshStandardMaterial color="white"/>
-                </mesh>
-            </Center>
+            {/* <OrbitControls 
+                makeDefault 
+                enabled={false}
+            >
+            </OrbitControls> */}
+        
+            <Float
+                speed={0.3} // Animation speed, defaults to 1
+                rotationIntensity={1} // XYZ rotation intensity, defaults to 1
+                floatIntensity={1} // Up/down float intensity, works like a multiplier with floatingRange,defaults to 1
+            >
+                <Center>
+                    <mesh geometry={nodes.stage.geometry}>
+                        <meshStandardMaterial map={stageDiffuse}/>
+                    </mesh>
+                    <mesh 
+                        geometry={mesh1.nodes.tv_screen.geometry}
+                        position={[-1.73, 1.13, 0.16]}
+                        rotation={[-1.58, 0.01, -1.57]}
+                        scale={[0.17, 1.05, 0.33]}
+                    >
+                        <meshBasicMaterial map={videoTexture} toneMapped={false} />
+                    </mesh>
+                    <mesh 
+                        geometry={mesh1.nodes.btn_base.geometry}
+                        //position={[-0.74, -0.28, 1.62]}
+                        position={[-0.74, -0.28, btnZPos]}
+                        rotation={[0, -1.57, 0 ]}
+                        scale={[0.6, 0.6, 0.6]}
+                        onClick={(e) => {
+                            // Handle click event
+                            console.log('Clicked on btn_base!');
+                            console.log("Camera Position:", camera.position);
+                            console.log("Camera Quaternion:", camera.quaternion);
+                        }}
+                        onPointerOver={(e) => {
+                            // Handle hover over event
+                            console.log('Mouse over btn_base');
+                            if (e.eventObject === mesh1.nodes.btn_base) {
+                                setHovered(true);
+                            }
+                        }}
+                        onPointerOut={(e) => {
+                            // Handle hover out event
+                            console.log('Mouse out of btn_base');
+                        }}
+                    >
+                        <meshStandardMaterial color="red"/>
+                    </mesh>
+                    <mesh 
+                        geometry={mesh1.nodes.btn_text.geometry}
+                        position={[-0.74, -0.28, 1.62]}
+                        rotation={[0, -1.57, 0 ]}
+                        scale={[0.6, 0.6, 0.6]}
+                    >
+                        <meshStandardMaterial color="white"/>
+                    </mesh>
+                </Center>
+            </Float>
+            
             <Environment 
                 background
                 preset="apartment"
@@ -101,25 +182,25 @@ export default function Experience() {
     );
 };
 
-function TextWithBackground({ position }) {
-    const [x, y, z] = position;
-    console.log(x, y, z); // This should now log the x, y, and z coordinates
+// function TextWithBackground({ position }) {
+//     const [x, y, z] = position;
+//     console.log(x, y, z); // This should now log the x, y, and z coordinates
   
-    return (
-      <>
-        <mesh position={[x, y+0.25, z]}>
-          <Text 
-            fontSize={0.2} 
-            color="#ffffff"
-          >
-            Axe
-          </Text>
-        </mesh>
-        <mesh position={[x,y+0.27,z - 0.01]} /* slightly behind the text */>
-        <planeGeometry args={[0.5, 0.2]} />
-        <meshBasicMaterial color="#000000" transparent opacity={0.7} />
-      </mesh>
+//     return (
+//       <>
+//         <mesh position={[x, y+0.25, z]}>
+//           <Text 
+//             fontSize={0.2} 
+//             color="#ffffff"
+//           >
+//             Axe
+//           </Text>
+//         </mesh>
+//         <mesh position={[x,y+0.27,z - 0.01]} /* slightly behind the text */>
+//         <planeGeometry args={[0.5, 0.2]} />
+//         <meshBasicMaterial color="#000000" transparent opacity={0.7} />
+//       </mesh>
 
-      </>
-    );
-  }
+//       </>
+//     );
+//   }
